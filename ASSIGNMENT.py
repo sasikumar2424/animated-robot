344d46f26e4178149ddab9d4f5500c203c2a6be7
sasikumar2424 <99247647+sasikumar2424@users.noreply.github.com>
2022-04-05 20:21:02 +0530

    Add files via upload

---
+++ ASSIGNMENT.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from tkinter import *
+from tkinter import ttk
+
+
+# In[2]:
+
+
+window = Tk()
+window.title("Welcome to TutorialsPoint")
+window.geometry('400x400')
+window.configure(background = "grey");
+
+
+# In[3]:
+
+
+a = Label(window ,text = "First Name").grid(row = 0,column = 0)
+b = Label(window ,text = "Last Name").grid(row = 1,column = 0)
+c = Label(window ,text = "Email Id").grid(row = 2,column = 0)
+d = Label(window ,text = "Contact Number").grid(row = 3,column = 0)
+a1 = Entry(window).grid(row = 0,column = 1)
+b1 = Entry(window).grid(row = 1,column = 1)
+c1 = Entry(window).grid(row = 2,column = 1)
+d1 = Entry(window).grid(row = 3,column = 1)
+def clicked():
+   res = "Welcome to " + txt.get()
+   lbl.configure(text= res)
+
+
+# In[ ]:
+
+
+btn = ttk.Button(window ,text="Submit").grid(row=4,column=0)
+window.mainloop()
+
+
+# In[ ]:
+
+
+
+
